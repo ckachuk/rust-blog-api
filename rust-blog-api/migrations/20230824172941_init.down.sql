48038de46a17ea82cac6acaaa6cf41ba559de91b
carlk <carloskachuk@gmail.com>
2023-08-25 08:58:14 -0300

    feat: db connection has been implemented

---
+++ rust-blog-api/migrations/20230824172941_init.down.sql
@@ -0,0 +1,6 @@
+-- Add down migration script here
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS credentials;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS comments;
