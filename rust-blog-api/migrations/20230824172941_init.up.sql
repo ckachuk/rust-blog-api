1cc4614334f0b217f0fd33f84ca61e4cb6a25063
ckachuk <62022941+ckachuk@users.noreply.github.com>
2023-08-25 08:59:13 -0300

    feat: db connection has been implemented (#1)

---
+++ rust-blog-api/migrations/20230824172941_init.up.sql
@@ -0,0 +1,59 @@
+
+-- Add up migration script here
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+
+CREATE TABLE
+    IF NOT EXISTS categories(
+        pk_category_id uuid PRIMARY KEY DEFAULT (uuid_generate_v4()),
+        category_name VARCHAR(100) NOT NULL UNIQUE
+    );
+
+CREATE TABLE
+    IF NOT EXISTS credentials(
+        pk_credential_id uuid PRIMARY KEY DEFAULT (uuid_generate_v4()),
+        is_author BOOLEAN DEFAULT 'f', 
+        is_admin BOOLEAN DEFAULT 'f'
+    );
+CREATE TABLE
+    IF NOT EXISTS users(
+        pk_user_id uuid PRIMARY KEY DEFAULT (uuid_generate_v4()),
+        credential_id uuid NOT NULL UNIQUE,
+        username VARCHAR(50) NOT NULL UNIQUE,
+        password VARCHAR(150) NOT NULL,
+        fullname VARCHAR (100) NOT NULL,
+        CONSTRAINT fk_credential_id FOREIGN KEY(credential_id)
+            REFERENCES credentials(pk_credential_id)
+    );
+CREATE TABLE
+    IF NOT EXISTS posts(
+        pk_post_id uuid PRIMARY KEY DEFAULT (uuid_generate_v4()),
+        user_id uuid NOT NULL UNIQUE,
+        category_id uuid NOT NULL UNIQUE,
+        title VARCHAR(255) NOT NULL,
+        body VARCHAR(255) NOT NULL,
+        CONSTRAINT fk_user_id FOREIGN KEY(user_id)
+            REFERENCES users(pk_user_id),
+        CONSTRAINT fk_category_id FOREIGN KEY(category_id)
+            REFERENCES categories(pk_category_id),
+        create_at TIMESTAMP 
+            WITH TIME ZONE DEFAULT NOW(),
+        update_at TIMESTAMP
+            WITH TIME ZONE DEFAULT NOW()
+    );
+
+CREATE TABLE
+    IF NOT EXISTS comments(
+        pk_comment_id uuid PRIMARY KEY DEFAULT (uuid_generate_v4()),
+        post_id uuid NOT NULL UNIQUE,
+        user_id uuid NOT NULL UNIQUE,
+        body VARCHAR(255) NOT NULL,
+        CONSTRAINT fk_post_id FOREIGN KEY(post_id)
+            REFERENCES posts(pk_post_id),
+        CONSTRAINT fk_user_id FOREIGN KEY(user_id)
+            REFERENCES users(pk_user_id),
+        create_at TIMESTAMP 
+            WITH TIME ZONE DEFAULT NOW(),
+        update_at TIMESTAMP
+            WITH TIME ZONE DEFAULT NOW()
+    )
--- rust-blog-api/src/main.rs
+++ rust-blog-api/src/main.rs
@@ -1,4 +1,12 @@
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+use dotenv::dotenv
+
 #[macro_use] extern crate rocket;
+
+pub struct App_state{
+    db: Pool<Postgres>,
+}
+
 
 #[get("/")]
 fn index() -> &'static str {
@@ -7,5 +15,25 @@ fn index() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
+    dotenv().ok()
     rocket::build().mount("/", routes![index])
+
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URl must be set")
+    let pool= match PgPoolOptions::new()
+        .max_connection(10)
+        .connect(&database_url)
+        .await
+    {
+            Ok(pool) => {
+                println!("✅Connection to the database is successful!");
+                pool
+            }
+            Err(err) => {
+                println!("🔥 Failed to connect to the database: {:?}", err);
+                std::process::exit(1);
+            }
+    };
+    
+
+    
 }
